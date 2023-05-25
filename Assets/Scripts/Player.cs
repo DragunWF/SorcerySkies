77c2a0770e34ed43da74d6449f20590b90e2d73a
dragunwf <plarisanmarc@gmail.com>
2023-05-25 10:51:32 +0800

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Animator _animator;
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
     private void Start()
     {
-
+        
     }
 
     private void Update()
     {
-
+        
     }
 }
