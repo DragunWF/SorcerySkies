5cf9cb05b631589ef8fa9429a7a24a3978b2d698
dragunwf <plarisanmarc@gmail.com>
2023-05-25 17:10:13 +0800

    Update Projectile.cs

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float[] _initialSpeedValues = { 3f, 3.5f, 3.75f, 4f, 4.25f, 4.5f, 5f };
+    protected float speed;
+    private Rigidbody2D _rigidbody;
+    private const float DESPAWN_POINT = -10; // For the y axis
+
+    private void Awake()
     {
-        
+        speed = _initialSpeedValues[Random.Range(0, _initialSpeedValues.Length)];
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, -speed);
+        if (transform.position.y <= DESPAWN_POINT)
+        {
+            Destroy(gameObject);
+        }
     }
+
 }
