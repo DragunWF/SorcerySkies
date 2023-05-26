253dd2e3c8dec3b72938639eeba4276279a17161
Marc Plarisan <plarisanmarc@gmail.com>
2023-05-26 21:49:26 +0800

    Update DamageProjectile.cs

--- Assets/Scripts/DamageProjectile.cs
+++ Assets/Scripts/DamageProjectile.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DamageProjectile : MonoBehaviour
+public class DamageProjectile : Projectile
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.tag == "Player")
+        {
+            FindObjectOfType<Player>().DamagePlayer();
+            Destroy(gameObject);
+        }
     }
 }
