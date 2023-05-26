5e45401ed27533e45c486056ee17098cda8b7b9b
Marc Plarisan <plarisanmarc@gmail.com>
2023-05-26 17:31:35 +0800

    Update MainSceneUI.cs

--- Assets/Scripts/MainSceneUI.cs
+++ Assets/Scripts/MainSceneUI.cs
@@ -8,14 +8,24 @@ public class MainSceneUI : MonoBehaviour
     private TextMeshProUGUI _scoreText;
     private TextMeshProUGUI _livesText;
 
+    public void UpdateScoreText(int value)
+    {
+        _scoreText.text = GetFormatText("Score", value);
+    }
+
+    public void UpdateLivesText(int value)
+    {
+        _livesText.text = GetFormatText("Lives", value);
+    }
+
     private void Awake()
     {
         _scoreText = GameObject.Find("ScoreText").GetComponent<TextMeshProUGUI>();
         _livesText = GameObject.Find("LivesText").GetComponent<TextMeshProUGUI>();
     }
 
-    private void Update()
+    private string GetFormatText(string header, int value)
     {
-
+        return string.Format("{0}: {1}", header, value);
     }
 }
