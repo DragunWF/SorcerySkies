6a78a9f116ca70b8e4c02d9064e6e5e1d32abcd0
dragunwf <plarisanmarc@gmail.com>
2023-05-27 23:28:04 +0800

    Update AudioPlayer.cs

--- Assets/Scripts/AudioPlayer.cs
+++ Assets/Scripts/AudioPlayer.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public sealed class AudioPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Dictionary<string, AudioClip> clips = new Dictionary<string, AudioClip>();
+    private Dictionary<string, float> volumes = new Dictionary<string, float>();
+
+    #region Play Audio File Methods
+
+    public void PlayDeath() => PlayClip("Death");
+    public void PlayJump() => PlayClip("Jump");
+    public void PlayPickup() => PlayClip("Pickup");
+    public void PlayDamage() => PlayClip("Damage");
+
+    #endregion
+
+    private void Awake()
     {
-        
+        clips.Add("Death", Resources.Load("Audio/Shoot") as AudioClip);
+        volumes.Add("Death", 0.75f);
+
+        clips.Add("Jump", Resources.Load("Audio/Jump") as AudioClip);
+        volumes.Add("Jump", 0.35f);
+
+        clips.Add("Pickup", Resources.Load("Audio/Pickup") as AudioClip);
+        volumes.Add("Pickup", 0.75f);
+
+        clips.Add("Damage", Resources.Load("Audio/Damage") as AudioClip);
+        volumes.Add("Damage", 0.55f);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void PlayClip(string fileName)
     {
-        
+        if (clips[fileName] != null)
+        {
+            Vector2 cameraPos = Camera.main.transform.position;
+            AudioSource.PlayClipAtPoint(clips[fileName], cameraPos, volumes[fileName]);
+        }
     }
 }
