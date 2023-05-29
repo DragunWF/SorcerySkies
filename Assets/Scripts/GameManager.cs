63dd83f0482eec8bcc0154a9ace9544d886a4dcc
Marc Plarisan <plarisanmarc@gmail.com>
2023-05-29 11:47:11 +0800

    Update GameManager.cs Fix bug where scene indices are not correct

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,15 +9,24 @@ public sealed class GameManager : MonoBehaviour
 
     private void Awake()
     {
-        scenes.Add("Main", 0);
-        scenes.Add("TitleScreen", 1);
+        scenes.Add("TitleScreen", 0);
+        scenes.Add("Main", 1);
         scenes.Add("HowToPlay", 2);
         scenes.Add("RetryMenu", 3);
     }
 
     #region Load Methods
 
-    public void LoadMainScene() => LoadScene(scenes["Main"]);
+    public void LoadMainScene()
+    {
+        GameState gameState = FindObjectOfType<GameState>();
+        if (gameState != null)
+        {
+            gameState.ResetState();
+        }
+        LoadScene(scenes["Main"]);
+    }
+
     public void LoadTitleScreen() => LoadScene(scenes["TitleScreen"]);
     public void LoadHowToPlay() => LoadScene(scenes["HowToPlay"]);
     public void LoadRetryMenu() => LoadScene(scenes["RetryMenu"]);
