541a4838c1726b5589077514493ad2bcfd939535
Marc Plarisan <plarisanmarc@gmail.com>
2023-05-29 11:04:22 +0800

    Update DifficultyScaling.cs

--- Assets/Scripts/DifficultyScaling.cs
+++ Assets/Scripts/DifficultyScaling.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DifficultyScaling : MonoBehaviour
+public sealed class DifficultyScaling : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private GameState _gameState;
+    private Spawner _spawner;
+    private float[] _difficulties = {
+        2.25f, 2f, 1.75f, 1.5f, 1.25f, 1f, 0.75f, 0.5f, 0.4f, 0.3f, 0.25f
+    };
+    private float _currentTimeToScale;
+
+    private void Awake()
     {
-        
+        _currentTimeToScale = _difficulties[0];
+        _gameState = FindObjectOfType<GameState>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        StartCoroutine(ScaleDifficulty());
+    }
+
+    private IEnumerator ScaleDifficulty()
+    {
+        const float delay = 0.5f;
+        yield return new WaitForSeconds(delay);
+
+        while (true)
+        {
+            _gameState.increaseDifficulty();
+            yield return new WaitForSeconds(_currentTimeToScale);
+        }
     }
 }
