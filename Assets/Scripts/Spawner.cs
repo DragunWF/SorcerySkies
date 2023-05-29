cd78f1e58185ed66de12be83dce3d361935176c1
Marc Plarisan <plarisanmarc@gmail.com>
2023-05-29 13:24:35 +0800

    Update Spawner.cs

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -14,7 +14,7 @@ public sealed class Spawner : MonoBehaviour
     private GameObject[] _damageProjectiles;
     private GameObject[] _lootProjectiles;
 
-    public void updateSpawnSpeed(int value) => _spawnSpeed = value;
+    public void UpdateSpawnSpeed(int value) => _spawnSpeed = value;
 
     private void Awake()
     {
